314cafb92a932fd861bbc611239cee259495be53
AnastasyiaMaximova <rsk.maximova@bk.ru>
2024-12-18 20:13:56 +0300

    initial commit

---
+++ ArtWorksAPP/ViewControllers/ArtWorksTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ArtWorksTableViewController.swift
+//  ArtWorksAPP
+//
+//  Created by Anastasya Maximova on 18.12.2024.
+//
+
+import UIKit
+
+final class ArtWorksTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
